076332e0f15b4090e5d923f3185351ce3e593643
Andrew Caniglia <andrewcaniglia@gmail.com>
2020-12-20 05:04:30 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -12,9 +12,9 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
-  def genres=(genres)
-    @genres = genres
-    GENRES << genres
+  def genres=(genre)
+    @genre = genre
+    GENRES << genre
   end
 
 end
